7a6f7368376b68c429f9cd41491f0281f43ed5f4
spmanolo <manolosan2001@gmail.com>
2024-10-26 22:00:01 +0200

    Update styling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
   return (
-    <div className="min-h-screen bg-slate-900 text-slate-100 flex flex-col justify-center items-center gap-5">
-      <a className="text-4xl">
+    <div className="min-h-screen bg-slate-900 text-slate-100 flex flex-col justify-items-center justify-center items-center gap-5">
+      <a className="lg:text-4xl md:text-3xl text-2xl">
         Hola, soy Manuel Sánchez Pérez
       </a>
-      <a className="text-lg">
+      <a className="text-lg md:text-xl">
         Software Developer
       </a>
     </div>

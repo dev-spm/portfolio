69616f4c7bda265248fe971cbb25e8fc7666eb53
spmanolo <manolosan2001@gmail.com>
2024-11-22 13:42:29 +0100

    Improve header visibility by adjusting z-index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from '/favicon.svg'
 
 export default function Header() {
   return (
-    <div className="sticky top-0 flex overflow-auto w-full md:w-4/5 lg:w-2/3 bg-slate-700 border
+    <div className="sticky z-10 top-0 flex overflow-auto w-full md:w-4/5 lg:w-2/3 bg-slate-700 border
                 border-transparent sm:rounded-full px-7 py-5 sm:mt-5 text-white text-center 
                 lg:text-xl md:text-base text-sm items-center justify-between">
       <img className="w-10" src={logo} />

cefd9d6ad3b11cf3de59b334a071ca48b286e349
spmanolo <manolosan2001@gmail.com>
2024-11-22 15:31:33 +0100

    Enhance dashboard and projects layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,9 +6,9 @@ export default function Dashboard() {
       <div className='flex flex-col my-16 sm:my-24 md:my-36 lg:my-48 md:h-full md:flex-row gap-7 md:gap-0
                     text-slate-100 justify-start items-center'>
         <div className="overflow-auto flex w-full sm:w-1/2 justify-center">
-          <img className="rounded-lg w-1/2 sm:w-3/5 sm:max-w-80 shadow-2xl" src="/img/perfil.jpg" />
+          <img className="rounded-lg w-1/2 max-w-60 sm:w-3/5 sm:max-w-72 sm:min-w-52 md:min-w-44 shadow-2xl" src="/img/perfil.jpg" />
         </div>
-        <div className="flex flex-col w-1/2 sm:pr-12 md:pr-28">
+        <div className="flex flex-col w-1/2 sm:pr-12 md:pr-28 lg:pr-48">
           <div className="lg:text-4xl md:text-3xl text-2xl mb-6">
             <a className="font-bold bg-gradient-to-r from-rose-900 to-slate-700 text-transparent bg-clip-text">
               ¡Hola!
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@
 
 export default function Projects() {
   return (
-    <div className="flex flex-col gap-10 justify-start w-full pt-10 px-14 md:px-24 h-full">
+    <div className="flex flex-col gap-10 justify-start w-full pt-10 px-14 md:px-20 lg:px-24 h-full">
       <div className="">
         <div className="text-3xl font-bold text-center text-slate-100">
           Proyectos
@@ -14,17 +14,17 @@ export default function Projects() {
       <div id="block" className="bg-slate-700 flex flex-col gap-5 md:flex-row p-5 md:p-14 rounded-lg 
                                 items-center md:items-start text-center md:text-start">
         <div className="flex flex-col w-full md:w-1/2 text-slate-50 items-center md:items-start">
-          <p className="text-lg mb-3 md:text-xl lg:text-2xl font-bold">
+          <p className="text-xl mb-3 md:text-2xl lg:text-2xl font-bold">
             Portfolio
           </p>
-          <p className="lg:text-lg">
+          <p className="sm:text-lg lg:text-xl">
             <a>Página web personal creada con </a>
             <a className="font-bold bg-slate-950 text-transparent bg-clip-text">React </a>
             <a>y </a>
             <a className="font-bold bg-slate-950 text-transparent bg-clip-text">TailwindCSS</a>
           </p>
         </div>
-        <div className="overflow-hidden border h-auto w-3/4 mb-3 md:mb-0 md:w-1/2 text-center">
+        <div className="overflow-hidden border rounded-lg h-auto w-3/4 mb-3 md:mb-0 md:w-1/2 text-center">
           <img className="w-full h-full object-cover" src="/img/portfolio.png" />
         </div>
       </div>
